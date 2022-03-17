99e649c9a8e316fd818081619a4b8aab7cc3c0d5
Aikeilol <garena3307@mail.ru>
2022-03-17 18:28:08 +0300

    add 2nd component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,72 @@
+import { Component } from 'react/cjs/react.production.min';
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+class CharList extends Component {
+    state = {
+        chars: [],
+        loading: true,
+    }
+
+    componentDidMount() {
+        this.updateCharList();
+    }
+
+    marvelService = new MarvelService();
+
+    updateCharList = () => {
+        this.setState({
+            loading: true,
+        })
+        this.marvelService
+            .getAllCharacters()
+            .then(res => {
+                this.setState({
+                    chars: res.map(char => [char.name, char.thumbnail]),
+                    loading: false
+                })
+            })
+    }
+
+    renderChars = (chars) => {
+        chars.map(char => {
+            return (
+                < li className="char__item" >
+                    <img src={char[1]} alt="abyss" />
+                    <div className="char__name">{char[0]}</div>
+                </li >
+            )
+        })
+    }
+
+    render() {
+        let { chars, loading } = this.state;
+
+        if (!loading) {
+            chars = chars.map(char => {
+                return (
+                    < li className="char__item" >
+                        <img src={char[1]} style={{ objectFit: char[1] === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? 'contain' : 'cover' }} alt="abyss" />
+                        <div className="char__name">{char[0]}</div>
+                    </li >
+                )
+            })
+        } else {
+            chars = <Spinner />
+        }
+
+        return (
+            <div className="char__list">
+                <ul className="char__grid" style={{ display: loading ? 'flex' : 'grid', justifyContent: 'center' }}>
+                    {chars}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div >
+        )
+    }
 }
 
 export default CharList;
